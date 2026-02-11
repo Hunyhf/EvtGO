8fb1a9d9191c8cb4c72f8e2113380cb3a184229b
h234h <huynhnguyen56392@gmail.com>
2026-02-12 04:30:07 +0700

    da vao duoc orgaziner

--- src/components/layouts/AdminLayout.jsx
+++ src/components/layouts/AdminLayout.jsx
@@ -1,23 +1,18 @@
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import Breadcrumb from '@components/Breadcrumb/Breadcrumb';
-import { Outlet } from 'react-router-dom'; // TƯ DUY SENIOR: Dùng Outlet để render con
+import { Outlet } from 'react-router-dom';
 
 function AdminLayout() {
     return (
         <div className='admin-layout'>
-            <Header />
             <main className='admin-content'>
                 <div className='container'>
-                    <Breadcrumb />
-                    {/* - Outlet sẽ render các component con từ routes/index.jsx */}
                     <Outlet />
                 </div>
             </main>
-            <Footer />
         </div>
     );
 }
 
-// LỖI CỦA BẠN LÀ THIẾU DÒNG NÀY
 export default AdminLayout;
--- src/components/layouts/OrganizerLayout.jsx
+++ src/components/layouts/OrganizerLayout.jsx
@@ -1,7 +1,7 @@
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import Breadcrumb from '@components/Breadcrumb/Breadcrumb';
-import { Outlet } from 'react-router-dom'; // TƯ DUY SENIOR: Dùng Outlet để render con
+import { Outlet } from 'react-router-dom';
 
 function OrganizerLayout() {
     return (
