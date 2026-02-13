9227c8cd155e05fbfca8c465ec003e0a028a706e
h234h <huynhnguyen56392@gmail.com>
2026-02-13 16:14:22 +0700

    tối ưu hiển thị trending cho mobile

--- src/pages/customer/Home/Home.jsx
+++ src/pages/customer/Home/Home.jsx
@@ -1,6 +1,7 @@
+import { useRef } from 'react';
+import classNames from 'classnames/bind';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import classNames from 'classnames/bind';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,10 +9,11 @@ import 'swiper/css/pagination';
 
 import styles from './Home.module.scss';
 import Nav from '@components/Nav/Nav.jsx';
-import EventCard from '@components/EventCard/EventCard.jsx';
 import { BANNER_DATA, TRENDING_DATA } from './constants';
 
 const cx = classNames.bind(styles);
+
+// Cấu hình Banner Swiper
 const swiperConfig = {
     modules: [Navigation, Pagination, Autoplay],
     spaceBetween: 20,
@@ -25,25 +27,35 @@ const swiperConfig = {
         1024: { slidesPerView: 2, spaceBetween: 30 }
     }
 };
+
 function Home() {
+    const trendingRef = useRef(null);
+
+    const handleScroll = direction => {
+        const { current } = trendingRef;
+        if (current) {
+            // Cuộn theo chiều rộng của khung hiển thị để mượt mà hơn
+            const scrollAmount = current.clientWidth;
+            if (direction === 'left') {
+                current.scrollLeft -= scrollAmount;
+            } else {
+                current.scrollLeft += scrollAmount;
+            }
+        }
+    };
+
     return (
         <main className={cx('home')}>
             <Nav />
 
             <div className={cx('wrapper')}>
                 {/* Banner Section */}
-                <section
-                    className={cx('banner-container')}
-                    aria-label='Featured Banners'
-                >
+                <section className={cx('banner-container')}>
                     <Swiper {...swiperConfig}>
                         {BANNER_DATA.map(banner => (
                             <SwiperSlide key={banner.id}>
                                 <div className={cx('banner-item')}>
-                                    <img
-                                        src={banner.url}
-                                        alt={`Promotion ${banner.id}`}
-                                    />
+                                    <img src={banner.url} alt='Banner' />
                                 </div>
                             </SwiperSlide>
                         ))}
@@ -56,13 +68,39 @@ function Home() {
                         <h3 className={cx('section-title')}>
                             Sự kiện đang xu hướng
                         </h3>
-                        {/* Có thể thêm nút "Xem tất cả" tại đây */}
                     </header>
 
-                    <div className={cx('event-grid')}>
-                        {TRENDING_DATA.map(item => (
-                            <EventCard key={item.id} data={item} />
-                        ))}
+                    {/* Container chứa cả list ảnh và nút bấm đè lên */}
+                    <div className={cx('trending-content')}>
+                        <button
+                            className={cx('control-btn', 'prev')}
+                            onClick={() => handleScroll('left')}
+                            aria-label='Previous'
+                        >
+                            ❮
+                        </button>
+
+                        <div
+                            className={cx('event-grid-manual')}
+                            ref={trendingRef}
+                        >
+                            {TRENDING_DATA.map(item => (
+                                <div
+                                    key={item.id}
+                                    className={cx('trending-img-wrapper')}
+                                >
+                                    <img src={item.url} alt={item.title} />
+                                </div>
+                            ))}
+                        </div>
+
+                        <button
+                            className={cx('control-btn', 'next')}
+                            onClick={() => handleScroll('right')}
+                            aria-label='Next'
+                        >
+                            ❯
+                        </button>
                     </div>
                 </section>
             </div>
